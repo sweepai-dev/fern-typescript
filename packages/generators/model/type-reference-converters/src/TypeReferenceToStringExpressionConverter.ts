75ac441ee7ab2b21181cde28a610de5905fb3e28
Zach Kirsch <zachkirsch@gmail.com>
2023-05-15 14:44:40 -0400

    Handle optional non-string headers

--- packages/generators/model/type-reference-converters/src/TypeReferenceToStringExpressionConverter.ts
+++ packages/generators/model/type-reference-converters/src/TypeReferenceToStringExpressionConverter.ts
@@ -2,6 +2,7 @@ import {
     ContainerType,
     DeclaredTypeName,
     Literal,
+    PrimitiveType,
     ResolvedTypeReference,
     ShapeType,
     TypeReference,
@@ -20,26 +21,11 @@ export class TypeReferenceToStringExpressionConverter extends AbstractTypeRefere
     protected override named(typeName: DeclaredTypeName): (reference: ts.Expression) => ExpressionReferenceNode {
         const resolvedType = this.typeResolver.resolveTypeName(typeName);
         return ResolvedTypeReference._visit<(reference: ts.Expression) => ExpressionReferenceNode>(resolvedType, {
-            container: (containerType) =>
-                ContainerType._visit(containerType, {
-                    list: this.list.bind(this),
-                    optional: this.optional.bind(this),
-                    set: this.set.bind(this),
-                    map: this.map.bind(this),
-                    literal: () => {
-                        throw new Error("Literals are unsupported!");
-                    },
-                    _unknown: () => {
-                        throw new Error("Unknown ContainerType: " + containerType._type);
-                    },
-                }),
+            container: this.container.bind(this),
             primitive: this.primitive.bind(this),
             named: ({ shape }) => {
                 if (shape === ShapeType.Enum) {
-                    return (reference) => ({
-                        expression: reference,
-                        isNullable: false,
-                    });
+                    return this.string();
                 }
                 if (shape === ShapeType.UndiscriminatedUnion) {
                     return this.jsonStringifyIfNotString.bind(this);
@@ -54,48 +40,32 @@ export class TypeReferenceToStringExpressionConverter extends AbstractTypeRefere
     }
 
     protected override string(): (reference: ts.Expression) => ExpressionReferenceNode {
-        return (reference) => ({ expression: reference, isNullable: false });
+        return (reference) => this.maybeOptionalString(reference, { isOptional: false });
     }
 
     protected override number(): (reference: ts.Expression) => ExpressionReferenceNode {
-        return (reference) => ({
-            expression: ts.factory.createCallExpression(
-                ts.factory.createPropertyAccessExpression(reference, "toString"),
-                undefined,
-                undefined
-            ),
-            isNullable: false,
-        });
+        return (reference) => this.maybeOptionalNumber(reference, { isOptional: false });
     }
 
     protected override boolean(): (reference: ts.Expression) => ExpressionReferenceNode {
-        return (reference) => ({
-            expression: ts.factory.createCallExpression(
-                ts.factory.createPropertyAccessExpression(reference, "toString"),
-                undefined,
-                undefined
-            ),
-            isNullable: false,
-        });
+        return (reference) => this.maybeOptionalBoolean(reference, { isOptional: false });
     }
 
     protected override dateTime(): (reference: ts.Expression) => ExpressionReferenceNode {
-        return (reference) => ({
-            expression: ts.factory.createCallExpression(
-                ts.factory.createPropertyAccessExpression(reference, "toISOString"),
-                undefined,
-                undefined
-            ),
-            isNullable: false,
-        });
+        return (reference) => this.maybeOptionalDateTime(reference, { isOptional: false });
     }
 
     protected override optional(itemType: TypeReference): (reference: ts.Expression) => ExpressionReferenceNode {
         return (reference) => {
-            return {
-                expression: this.convert(itemType)(reference).expression,
-                isNullable: true,
-            };
+            return TypeReference._visit<ExpressionReferenceNode>(itemType, {
+                named: (typeName) => this.optionalNamed(typeName, reference),
+                primitive: (primitive) => this.optionalPrimitive(primitive, reference),
+                container: (container) => this.optionalContainer(container, reference),
+                unknown: () => this.optionalUnknown(reference),
+                _unknown: () => {
+                    throw new Error("Unexpected type reference: " + itemType._type);
+                },
+            });
         };
     }
 
@@ -132,6 +102,17 @@ export class TypeReferenceToStringExpressionConverter extends AbstractTypeRefere
     }
 
     private jsonStringify(reference: ts.Expression): ExpressionReferenceNode {
+        return this.maybeOptionalJsonStringify(reference, { isOptional: false });
+    }
+
+    private jsonStringifyIfNotString(reference: ts.Expression): ExpressionReferenceNode {
+        return this.maybeOptionalJsonStringifyIfNotString(reference, { isOptional: false });
+    }
+
+    private maybeOptionalJsonStringify(
+        reference: ts.Expression,
+        { isOptional }: { isOptional: boolean }
+    ): ExpressionReferenceNode {
         return {
             expression: ts.factory.createCallExpression(
                 ts.factory.createPropertyAccessExpression(
@@ -141,11 +122,14 @@ export class TypeReferenceToStringExpressionConverter extends AbstractTypeRefere
                 undefined,
                 [reference]
             ),
-            isNullable: false,
+            isNullable: isOptional,
         };
     }
 
-    private jsonStringifyIfNotString(reference: ts.Expression): ExpressionReferenceNode {
+    private maybeOptionalJsonStringifyIfNotString(
+        reference: ts.Expression,
+        { isOptional }: { isOptional: boolean }
+    ): ExpressionReferenceNode {
         return {
             expression: ts.factory.createConditionalExpression(
                 ts.factory.createBinaryExpression(
@@ -158,7 +142,166 @@ export class TypeReferenceToStringExpressionConverter extends AbstractTypeRefere
                 ts.factory.createToken(ts.SyntaxKind.ColonToken),
                 this.jsonStringify(reference).expression
             ),
+            isNullable: isOptional,
+        };
+    }
+
+    private optionalNamed(typeName: DeclaredTypeName, reference: ts.Expression): ExpressionReferenceNode {
+        const resolvedType = this.typeResolver.resolveTypeName(typeName);
+        return ResolvedTypeReference._visit<ExpressionReferenceNode>(resolvedType, {
+            container: (container) => this.optionalContainer(container, reference),
+            primitive: (primitive) => this.optionalPrimitive(primitive, reference),
+            named: ({ shape }) => {
+                if (shape === ShapeType.Enum) {
+                    return this.optionalString(reference);
+                }
+                if (shape === ShapeType.UndiscriminatedUnion) {
+                    return this.optionalJsonStringifyIfNotString(reference);
+                }
+                return this.optionalJsonStringify(reference);
+            },
+            unknown: () => this.optionalUnknown(reference),
+            _unknown: () => {
+                throw new Error("Unknown ResolvedTypeReference: " + resolvedType._type);
+            },
+        });
+    }
+
+    private optionalContainer(container: ContainerType, reference: ts.Expression): ExpressionReferenceNode {
+        return ContainerType._visit(container, {
+            list: () => this.optionalList(reference),
+            optional: (itemType) => this.optional(itemType)(reference),
+            set: () => this.optionalSet(reference),
+            map: () => this.optionalMap(reference),
+            literal: (literal) => this.optionalLiteral(literal, reference),
+            _unknown: () => {
+                throw new Error("Unknown ContainerType: " + container._type);
+            },
+        });
+    }
+
+    private optionalUnknown(reference: ts.Expression): ExpressionReferenceNode {
+        return {
+            expression: this.optionalJsonStringifyIfNotString(reference).expression,
+            isNullable: true,
+        };
+    }
+
+    private optionalList(reference: ts.Expression): ExpressionReferenceNode {
+        return {
+            expression: this.optionalJsonStringify(reference).expression,
+            isNullable: true,
+        };
+    }
+
+    private optionalSet(reference: ts.Expression): ExpressionReferenceNode {
+        return {
+            expression: this.optionalJsonStringify(reference).expression,
+            isNullable: true,
+        };
+    }
+
+    private optionalMap(reference: ts.Expression): ExpressionReferenceNode {
+        return {
+            expression: this.optionalJsonStringify(reference).expression,
+            isNullable: true,
+        };
+    }
+
+    private optionalLiteral(literal: Literal, reference: ts.Expression): ExpressionReferenceNode {
+        return {
+            expression: this.literal(literal)(reference).expression,
+            isNullable: true,
+        };
+    }
+
+    private optionalPrimitive(primitive: PrimitiveType, reference: ts.Expression): ExpressionReferenceNode {
+        return PrimitiveType._visit<ExpressionReferenceNode>(primitive, {
+            boolean: () => this.maybeOptionalBoolean(reference, { isOptional: true }),
+            double: () => this.maybeOptionalNumber(reference, { isOptional: true }),
+            integer: () => this.maybeOptionalNumber(reference, { isOptional: true }),
+            long: () => this.maybeOptionalNumber(reference, { isOptional: true }),
+            string: () => this.optionalString(reference),
+            uuid: () => this.optionalString(reference),
+            dateTime: () => this.maybeOptionalDateTime(reference, { isOptional: true }),
+            date: () => this.optionalString(reference),
+            base64: () => this.optionalString(reference),
+            _unknown: () => {
+                throw new Error("Unexpected primitive type: " + primitive);
+            },
+        });
+    }
+
+    private optionalString(reference: ts.Expression): ExpressionReferenceNode {
+        return this.maybeOptionalString(reference, { isOptional: true });
+    }
+
+    private maybeOptionalString(
+        reference: ts.Expression,
+        { isOptional }: { isOptional: boolean }
+    ): ExpressionReferenceNode {
+        return { expression: reference, isNullable: isOptional };
+    }
+
+    private maybeOptionalNumber(
+        reference: ts.Expression,
+        { isOptional }: { isOptional: boolean }
+    ): ExpressionReferenceNode {
+        return {
+            expression: ts.factory.createCallExpression(
+                ts.factory.createPropertyAccessChain(
+                    reference,
+                    isOptional ? ts.factory.createToken(ts.SyntaxKind.QuestionDotToken) : undefined,
+                    "toString"
+                ),
+                undefined,
+                undefined
+            ),
+            isNullable: isOptional,
+        };
+    }
+
+    private maybeOptionalBoolean(
+        reference: ts.Expression,
+        { isOptional }: { isOptional: boolean }
+    ): ExpressionReferenceNode {
+        return {
+            expression: ts.factory.createCallExpression(
+                ts.factory.createPropertyAccessChain(
+                    reference,
+                    isOptional ? ts.factory.createToken(ts.SyntaxKind.QuestionDotToken) : undefined,
+                    "toString"
+                ),
+                undefined,
+                undefined
+            ),
+            isNullable: isOptional,
+        };
+    }
+
+    private maybeOptionalDateTime(
+        reference: ts.Expression,
+        { isOptional }: { isOptional: boolean }
+    ): ExpressionReferenceNode {
+        return {
+            expression: ts.factory.createCallExpression(
+                ts.factory.createPropertyAccessChain(
+                    reference,
+                    isOptional ? ts.factory.createToken(ts.SyntaxKind.QuestionDotToken) : undefined,
+                    "toISOString"
+                ),
+                undefined,
+                undefined
+            ),
             isNullable: false,
         };
+    }
+
+    private optionalJsonStringify(reference: ts.Expression): ExpressionReferenceNode {
+        return this.maybeOptionalJsonStringify(reference, { isOptional: false });
+    }
+
+    private optionalJsonStringifyIfNotString(reference: ts.Expression): ExpressionReferenceNode {
+        return this.maybeOptionalJsonStringifyIfNotString(reference, { isOptional: false });
     }
 }
